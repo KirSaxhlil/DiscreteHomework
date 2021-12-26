ef6d22760dc0b9e572f6aabab0378b512fc5050f
KirSaxhlil <dimapisarenko28@gmail.com>
2021-12-26 15:28:37 +0300

    Created common abstract class

---
+++ Task1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombClasses
+{
+    abstract class CombClass
+    {
+        char[] set;
+        char[] obj;
+
+        abstract public void nextObj();
+    }
+}
