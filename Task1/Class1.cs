0ac3a9e760b1d7a73c700bc8e1b2689e13ea146c
KirSaxhlil <dimapisarenko28@gmail.com>
2021-12-26 15:39:58 +0300

    Created Arrangement with repetitions class

--- Task1/Class1.cs
+++ Task1/Class1.cs
@@ -8,9 +8,54 @@ namespace CombClasses
 {
     abstract class CombClass
     {
-        char[] set;
-        char[] obj;
+        protected char[] set;
+        protected char[] obj;
 
         abstract public void nextObj();
+
+        public string GetObj()
+        {
+            string str = "";
+            for (int i = 0; i < obj.Length; i++) str += obj[i];
+            return str;
+        }
+    }
+
+    class ArrangementR: CombClass
+    {
+        int k;
+
+        public ArrangementR(char[] set, int k)
+        {
+            this.set = set;
+            this.k = k;
+            obj = new char[k];
+            for (int i = 0; i < k; k++) obj[i] = set[0];
+        }
+
+        public override void nextObj()
+        {
+            int i = k - 1;
+            while (nextElem(ref obj[i--]));
+        }
+
+        protected bool nextElem(ref char elem)
+        {
+            int index = 0;
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (set[i] == elem) index = i;
+            }
+            if (index == set.Length - 1)
+            {
+                elem = set[0];
+                return true;
+            }
+            else
+            {
+                elem = set[index + 1];
+                return false;
+            }
+        }
     }
 }
