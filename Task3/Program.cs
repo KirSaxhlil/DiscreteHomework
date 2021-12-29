dbb387e93eea8aecdb338770713a19837a55ffed
KirSaxhlil <dimapisarenko28@gmail.com>
2021-12-29 03:02:10 +0300

    Created and configured Task3 project.

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombClasses;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
