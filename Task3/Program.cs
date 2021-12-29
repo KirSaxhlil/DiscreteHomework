4a241055403018ff00b4a80da04071646e966371
KirSaxhlil <dimapisarenko28@gmail.com>
2021-12-29 03:15:23 +0300

    Check function

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,6 +10,23 @@ namespace Task3
 {
     class Program
     {
+        static char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f' };
+        static bool Check(ArrangementR obj, int r)
+        {
+            int[] q = new int[alphabet.Length];
+            for (int i = 0; i < obj.GetObj().Length; i++)
+            {
+                q[obj.GetIndOfChar(obj.GetObj()[i])]++;
+            }
+            int q2 = 0, qR = 0;
+            for (int i = 0; i < obj.GetObj().Length; i++)
+            {
+                if (q[i] > 1) qR++;
+                if (q[i] == 2) q2++;
+            }
+
+            return q2 == qR && q2 == r;
+        }
         static void Main(string[] args)
         {
         }
