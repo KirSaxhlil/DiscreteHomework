8ba848571ffbd022db9613883e0d0db7699ff9f3
KirSaxhlil <dimapisarenko28@gmail.com>
2021-12-29 03:19:42 +0300

    Generating and output

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -19,7 +19,7 @@ namespace Task3
                 q[obj.GetIndOfChar(obj.GetObj()[i])]++;
             }
             int q2 = 0, qR = 0;
-            for (int i = 0; i < obj.GetObj().Length; i++)
+            for (int i = 0; i < alphabet.Length; i++)
             {
                 if (q[i] > 1) qR++;
                 if (q[i] == 2) q2++;
@@ -29,6 +29,24 @@ namespace Task3
         }
         static void Main(string[] args)
         {
+            StreamWriter output = new StreamWriter("One Repetitions.txt");
+            ArrangementR obj = new ArrangementR(alphabet, 5);
+            if (Check(obj, 1)) output.WriteLine(obj.GetObj());
+            while (obj.GetObj() != obj.GetLastObj())
+            {
+                obj.nextObj();
+                if (Check(obj, 1)) output.WriteLine(obj.GetObj());
+            }
+            output.Close();
+            output = new StreamWriter("Two Repetitions.txt");
+            obj = new ArrangementR(alphabet, 6);
+            if (Check(obj, 2)) output.WriteLine(obj.GetObj());
+            while (obj.GetObj() != obj.GetLastObj())
+            {
+                obj.nextObj();
+                if (Check(obj, 2)) output.WriteLine(obj.GetObj());
+            }
+            output.Close();
         }
     }
 }
